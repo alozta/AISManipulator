f479048aa9ab92b569f7a4ba47f3b40bb1f95b37
alozta <alozta@gmail.com>
2016-11-30 22:14:23 +0300

    Setting up searching routes by area

--- src/main/webapp/scripts/mapFeatures.js
+++ src/main/webapp/scripts/mapFeatures.js
@@ -16,6 +16,8 @@ function drawCircle() {
                 fillColor: '#ffffff',
                 fillOpacity: 0.5
             });
+            //console.log(lat+','+lng+','+parseInt(radius));
+            //console.log(getEstimatedCoordinates(lat,lng,parseInt(radius)));
             circle.addTo(mymap);
             setHoverPopup(circle, "Area " + ++areaCounter);
             circle.on('click', function(){
@@ -27,6 +29,9 @@ function drawCircle() {
     });
 }
 
+/*
+ * Set a hover popup for map objects to identify themselves.
+ */
 function setHoverPopup(layer, message) {
     layer.bindPopup(message);
     layer.on('mouseover', function (e) {
@@ -35,4 +40,45 @@ function setHoverPopup(layer, message) {
     layer.on('mouseout', function (e) {
         this.closePopup();
     });
+}
+
+function getRoutesInsideAnArea(area,start,end){
+    for(var i=0; i<3588; ++i){
+        //server call for mmsi
+        //foreach mmsi x,y call isInside
+        //if satisfied amount of x,y is inside add mmsi route to the print list
+    }
+}
+
+/*
+ * Calculates estimated values (EARTH IS NOT FLAT)
+ * @param center (lat,lon) json array
+ * @param radius radius in meters
+ * @return Square shape area coordinates in json array (more corner is better, i'll go with this one, also the map shows a circle that's why i narrow the estimation a bit more)
+ */
+function getEstimatedCoordinates(lat,lon,radius){
+    var area = [];
+    var r = (radius-70)/1000;                //trimming by N meters
+
+    area.push({'lat':lat+r, 'lon':lon+r});
+    area.push({'lat':lat+r, 'lon':lon-r});
+    area.push({'lat':lat-r, 'lon':lon+r});
+    area.push({'lat':lat-r, 'lon':lon-r});
+
+    return area;
+}
+
+/*
+ * @return if the coordinate is inside of the estimated area.
+ */
+function isInside(area,lat,lon){
+    if( area[0].lat > lat && area[0].lon > lon &&
+        area[1].lat > lat && area[1].lon < lon &&
+        area[2].lat < lat && area[2].lon > lon &&
+        area[3].lat < lat && area[3].lon < lon){
+
+            return true;
+        }
+
+    return false;
 }
