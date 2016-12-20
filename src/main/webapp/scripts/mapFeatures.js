857086612bda6a8cad764d617fb850e4b0a365b7
alozta <alozta@gmail.com>
2016-12-21 00:31:04 +0300

    Function that returns routes that passes through the selected areas

--- src/main/webapp/scripts/mapFeatures.js
+++ src/main/webapp/scripts/mapFeatures.js
@@ -5,6 +5,76 @@ var areaCounter = 0;
 var routeList = [];
 var mmsiList = [];
 
+//expects two area selection on the map and returns/shows day by day routes hits for both areas.
+function _2AreaExamine(){
+	var area1 = null, area2 = null;                                 //select areas
+	var result = [];
+	$('#mapid').one('click',function(e){
+		var r1 = prompt("Enter radius in meters", "");
+		area1 = getAreaLimit([lat,lng][0],[lat,lng][1],r1);
+
+		$('#mapid').one('click',function(e){
+			var r2 = prompt("Enter radius in meters", "");
+			area2 = getAreaLimit([lat,lng][0],[lat,lng][1],r2);
+
+			//date format: 2016-12-01, data is from 2016-10-10 to 2016-11-17 (both inclusive)
+			for(var i=10; i<31; ++i){                                       //october
+				var start = '2016-10-'+i;
+				var end = '2016-10-'+(i+1);
+				for(var j=0; j<3588; ++j){                                  //for each mmsi
+					routeList = [];
+					getVesselInfo(1,distinct_vessel_mmsi[j],start,end);     //send query for the mmsi
+					while(routeList.length != 1);                           //wait for the answer
+					for(var k=0; k<routeList[0].length; ++k){
+						if(isInside(area1,routeList[0][k][0],routeList[0][k][1]) && isInside(area2,routeList[0][k][0],routeList[0][k][1])){
+							result.push({'date':start, 'mmsi':distinct_vessel_mmsi[j]});
+							/*var selectedRoutePolyline = new L.Polyline(routeList[0], {  //draw the route
+								color: getRandomColor(),
+								weight: 3,
+								opacity: 0.5,
+								smoothFactor: 1
+							});
+							selectedRoutePolyline.addTo(mymap);
+							setHoverPopup(selectedRoutePolyline, distinct_vessel_mmsi[j]);
+							selectedRoutePolyline.on('click', function(){
+								mymap.removeLayer(selectedRoutePolyline);
+							});*/
+							break;
+						}
+					}
+				}
+			}
+			for(var i=1; i<=17; ++i){                                       //november
+				var start = '2016-11-'+i;
+                var end = '2016-11-'+(i+1);
+                for(var j=0; j<3588; ++j){                                  //for each mmsi
+                	routeList = [];
+                	getVesselInfo(1,distinct_vessel_mmsi[j],start,end);     //send query for the mmsi
+                	while(routeList.length != 1);                           //wait for the answer
+                	for(var k=0; k<routeList[0].length; ++k){
+                		if(isInside(area1,routeList[0][k][0],routeList[0][k][1]) && isInside(area2,routeList[0][k][0],routeList[0][k][1])){
+                			result.push({'date':start, 'mmsi':distinct_vessel_mmsi[j]});
+                			/*var selectedRoutePolyline = new L.Polyline(routeList[0], {  //draw the route
+                				color: getRandomColor(),
+                				weight: 3,
+                				opacity: 0.5,
+                				smoothFactor: 1
+                			});
+                			selectedRoutePolyline.addTo(mymap);
+                			setHoverPopup(selectedRoutePolyline, distinct_vessel_mmsi[j]);
+                			selectedRoutePolyline.on('click', function(){
+                			    mymap.removeLayer(selectedRoutePolyline);
+                		    });*/
+                		    break;
+                	    }
+                    }
+                }
+			}
+		});
+	});
+	return result;
+}
+
 /**
  * Draws a circle on a map.
  */
@@ -26,6 +96,7 @@ function drawCircle() {
 			console.log(lat+','+lng+','+parseInt(radius));
 			var start = document.getElementById("startDate").value;
 			var end = document.getElementById("endDate").value;
+			console.log(end);
 			var intersectedRoutes;
 			if(start=='' && end == '') intersectedRoutes = getRoutesInsideAnArea(getAreaLimit(lat,lng,parseInt(radius)),"2013","2017");
 			else if(start=='') intersectedRoutes = getRoutesInsideAnArea(getAreaLimit(lat,lng,parseInt(radius)),"2013",end);
@@ -57,7 +128,7 @@ function setHoverPopup(layer, message) {
  */
 function getRoutesInsideAnArea(area,start,end){
 	var reactionThreshold = 100;            //bigger the value less sensitive the noise
-	var routeLimit = 1000;                    //browser memory limitations
+	var routeLimit = 1000;                  //browser memory limitations
 	for(var i=0; i<3588; ++i){
 		getVesselInfo(1,distinct_vessel_mmsi[i],start,end);
 
